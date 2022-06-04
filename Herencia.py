3ea09c9e0198adc90bffbce022fc926e2111f0ea
Julianbuenhombre <104658843+Julianbuenhombre@users.noreply.github.com>
2022-06-03 20:24:56 -0500

    Create Herencia.py

---
+++ Herencia.py
@@ -0,0 +1,64 @@
+class Vehiculos():
+def _init_(self, marca, modelo):
+
+self.marca=marca
+self.modelo=modelo
+self.enmarcha=False	
+self.acelera=False
+self.frena=False
+
+def arrancar(self):
+    	
+    self.enmarcha=True
+
+def acelerar(self):
+
+    self.acelera=True
+
+ def frenar(self):
+    self.frena=True 
+
+def estado(self):
+    print ("Marca; ", self.marca, "\nModelo: ", self.modelo, "\EnMarcha: " ,
+       self.enmarcha, "\nAcelerando: ", self.acelera, "\nFrenando:" , seld.frena)
+
+    class Furgoneta(Vehiculos):
+        def carga(self, cargar):
+        	self.cargado=cargar
+        	if(self.cargado):
+        		return "La furgoneta esta cargada"
+
+        	else:
+
+        	    return "La furgoneta no esta cargada"
+
+    class Moto(Vehiculos):
+    	hcaballito=""
+    	def caballito(self):
+    		self.hcaballito="Voy haciendo el caballito"
+
+        def estado(self):
+        	print ("Marca: ", self.marca, "\nModelo: ", self.modelo, "\nEn Marcha: ",
+        		 self.enmarcha, "\nAcelerando: ", self.acelera, "\nFrenando: " , self.frena, "\n", self.hcaballito)
+
+class VElectronicos(Vehiculos):
+	def_init_(self, marca, modelo):
+	  self.autonomia=100
+
+	 def cargarEnergia(self):
+	     self.cargando=True
+
+    miMoto=Moto("Honda", "CBR")
+
+    miMoto.caballito()
+    miMoto.estado()
+    miFurgoneta=Furgoneta("Renault", "Kangoo")
+    miFurgoneta.arrancar()
+    miFurgoneta.estado()
+
+    print(miFurgoneta.carga(True))
+    class BicicletaElectrica(VElectricos,Vehuculos):
+
+    	pass
+
+    miBici=BicicletaElectrica()
